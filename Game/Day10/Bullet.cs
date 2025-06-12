3d49acebd926b80b8088c05c436770138c3d5f53
Jalaj Singhal <115914764+jalaj-25@users.noreply.github.com>
2025-06-12 22:00:37 +0530

    new updateds

---
+++ Game/Day10/Bullet.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 10f;
+    public float explosionRadius = 0f;
+    public GameObject impactEffect;
+
+    void Update()
+    {
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if (dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+        transform.LookAt(target);
+    }
+
+    void HitTarget()
+    {
+        GameObject effectIns = Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effectIns, 5f);
+        Debug.Log("We hit something!!");
+
+        if (explosionRadius > 0f)
+        {
+            Explode();
+        }
+        else
+        {
+            Damage(target);
+        }
+
+        Destroy(gameObject);
+    }
+
+    void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (Collider collider in colliders)
+        {
+            if (collider.CompareTag("Enemy"))
+            {
+                Damage(collider.transform);
+            }
+        }
+    }
+
+    void Damage(Transform enemy)
+    {
+        Destroy(enemy.gameObject);
+    }
+
+    public void Seek(Transform _target)
+    {
+        target = _target;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+    }
+}
---
+++ Game/Day10/Node.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+    public Vector3 positionOffset;
+
+    [Header("Optional")]
+    public GameObject turret;
+    
+    private Renderer rend;
+    private Color startColor;
+    public Color notEnoghMoneyColor;
+
+    private BuildManager buildManager;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+
+        // ✅ Initialize buildManager
+        buildManager = BuildManager.instance;
+    }
+
+    public Vector3 GetBuildPosition()
+    {
+        return transform.position + positionOffset;
+    }
+    void OnMouseDown()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+
+        if (!buildManager.CanBuild)
+            return;
+
+        if (turret != null)
+        {
+            Debug.Log("We can't build here!!");
+            return;
+        }
+
+        buildManager.BuildTurretOn(this);
+    }
+
+
+
+    void OnMouseEnter()
+    {
+        if (EventSystem.current.IsPointerOverGameObject()) // ✅ Correct
+        {
+            return;
+        }
+        if (!buildManager.CanBuild)
+            return;
+        if(buildManager.HasMoney)
+        {
+            rend.material.color = hoverColor;
+        }
+        else
+        {
+            rend.material.color = notEnoghMoneyColor;   
+        }
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
