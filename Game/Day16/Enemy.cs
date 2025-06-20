a2474e206aa6987003c9e102e53f61389752f777
Jalaj Singhal <115914764+jalaj-25@users.noreply.github.com>
2025-06-20 23:55:38 +0530

    new updates in UI

---
+++ Game/Day16/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [HideInInspector]
+    public float speed;
+    public float health = 100f;
+    public int worth = 50;
+    public GameObject DeathEffect;
+    public float startSpeed = 10f;
+
+    void Start()
+    {
+        speed = startSpeed;  // Initialize speed when enemy spawns
+    }
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Slow(float pct)
+    {
+        float newSpeed = startSpeed * (1f - pct);
+        if (speed > newSpeed)
+        {
+            speed = newSpeed;
+        }
+    }
+
+    void Die()
+    {
+        PlayerStats.Money += worth;
+        GameObject effect = Instantiate(DeathEffect, transform.position, transform.rotation);
+        Destroy(effect, 5f);
+        Destroy(gameObject);
+    }
+}
---
+++ Game/Day16/Node.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+    public Vector3 positionOffset;
+
+    [Header("Optional")]
+    public GameObject turret;
+    
+    private Renderer rend;
+    private Color startColor;
+    public Color notEnoghMoneyColor;
+
+    private BuildManager buildManager;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+
+        // ✅ Initialize buildManager
+        buildManager = BuildManager.instance;
+    }
+
+    public Vector3 GetBuildPosition()
+    {
+        return transform.position + positionOffset;
+    }
+    void OnMouseDown()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+
+        if (turret != null)
+        {
+            buildManager.SelectNode(this);
+            return;
+        }
+
+        if (!buildManager.CanBuild)
+            return;
+        BuildTurret(buildManager.GetTurretToBuild());
+    }
+
+    void BuildTurret(TurretBlueprint blueprint)
+    {
+
+        if (PlayerStats.Money < blueprint.cost)
+        {
+            Debug.Log("Not enogh paisaaa.");
+            return;
+        }
+
+        PlayerStats.Money -= blueprint.cost;
+        GameObject _turret = Instantiate(blueprint.prefab, GetBuildPosition(), Quaternion.identity);
+        turret = _turret;
+        GameObject effect = Instantiate(buildManager.buildEffect, GetBuildPosition(), Quaternion.identity);
+        Destroy(effect, 5f);
+        Debug.Log("Turret build");
+    }
+
+    void OnMouseEnter()
+    {
+        if (EventSystem.current.IsPointerOverGameObject()) // ✅ Correct
+        {
+            return;
+        }
+        if (!buildManager.CanBuild)
+            return;
+        if(buildManager.HasMoney)
+        {
+            rend.material.color = hoverColor;
+        }
+        else
+        {
+            rend.material.color = notEnoghMoneyColor;   
+        }
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
