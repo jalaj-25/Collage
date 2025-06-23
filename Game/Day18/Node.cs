93f92d9416f05c2768669b45b105ceb4cfb89d71
Jalaj Singhal <115914764+jalaj-25@users.noreply.github.com>
2025-06-23 21:22:02 +0530

    sell button working

---
+++ Game/Day18/Node.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+    public Vector3 positionOffset;
+
+    [HideInInspector]
+    public GameObject turret;
+    [HideInInspector]
+    public TurretBlueprint turretBlueprint;
+    [HideInInspector]
+    public bool isUpgraded = false;
+
+    private Renderer rend;
+    private Color startColor;
+    public Color notEnoghMoneyColor;
+
+    private BuildManager buildManager;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+
+        // ✅ Initialize buildManager
+        buildManager = BuildManager.instance;
+    }
+
+    public Vector3 GetBuildPosition()
+    {
+        return transform.position + positionOffset;
+    }
+    void OnMouseDown()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+
+        if (turret != null)
+        {
+            buildManager.SelectNode(this);
+            return;
+        }
+
+        if (!buildManager.CanBuild)
+            return;
+        BuildTurret(buildManager.GetTurretToBuild());
+    }
+
+    void BuildTurret(TurretBlueprint blueprint)
+    {
+
+        if (PlayerStats.Money < blueprint.cost)
+        {
+            Debug.Log("Not enogh paisaaa.");
+            return;
+        }
+
+        PlayerStats.Money -= blueprint.cost;
+        GameObject _turret = Instantiate(blueprint.prefab, GetBuildPosition(), Quaternion.identity);
+        turret = _turret;
+
+        turretBlueprint = blueprint;
+        GameObject effect = Instantiate(buildManager.buildEffect, GetBuildPosition(), Quaternion.identity);
+        Destroy(effect, 5f);
+        Debug.Log("Turret build");
+    }
+
+    public void UpgradeTurret()
+    {
+        if (PlayerStats.Money < turretBlueprint.upgradeCost)
+        {
+            Debug.Log("Not enogh paisaaa.");
+            return;
+        }
+
+        PlayerStats.Money -= turretBlueprint.upgradeCost;
+        //old turret destruction
+        Destroy(turret);
+
+        //building new turret
+
+        GameObject _turret = Instantiate(turretBlueprint.upgradedPrefab, GetBuildPosition(), Quaternion.identity);
+        turret = _turret;
+        GameObject effect = Instantiate(buildManager.buildEffect, GetBuildPosition(), Quaternion.identity);
+        Destroy(effect, 5f);
+        isUpgraded = true;
+        Debug.Log("Turret Upgradded");
+    }
+
+    public void SellTurret()
+    {
+        //effect for sell
+        GameObject effect = Instantiate(buildManager.sellEffect, GetBuildPosition(), Quaternion.identity);
+        Destroy(effect, 5f);
+
+        PlayerStats.Money += turretBlueprint.GetSellAmount(); 
+        Destroy(turret);
+
+        turretBlueprint = null;
+    }
+
+    void OnMouseEnter()
+    {
+        if (EventSystem.current.IsPointerOverGameObject()) // ✅ Correct
+        {
+            return;
+        }
+        if (!buildManager.CanBuild)
+            return;
+        if(buildManager.HasMoney)
+        {
+            rend.material.color = hoverColor;
+        }
+        else
+        {
+            rend.material.color = notEnoghMoneyColor;   
+        }
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
---
+++ Game/Day18/TurretBlueprint.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+[System.Serializable]
+public class TurretBlueprint
+{
+    public GameObject prefab;
+    public int cost;
+
+    public int upgradeCost;
+    public GameObject upgradedPrefab;
+
+    public int GetSellAmount()
+    {
+        return cost / 2;
+    }
+}
