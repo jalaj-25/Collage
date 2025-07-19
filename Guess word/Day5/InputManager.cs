a17b3f8f09359fb12afa939d9c3e1381310882a2
Jalaj Singhal <115914764+jalaj-25@users.noreply.github.com>
2025-07-19 20:11:50 +0530

    new updates

---
+++ Guess word/Day5/InputManager.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+
+public class InputManager : MonoBehaviour
+{
+    [Header("Element")]
+    [SerializeField] private WordContainer[] wordContainers;
+    [SerializeField] private Button tryButton;
+    [SerializeField] private KeyboardColorizer keyboardColorizer;
+
+    [Header("Settings")]
+    private int currentWordContainerIndex = 0;
+    private bool canAddLetter = true;
+
+    void Start()
+    {
+        Initialize();
+        KeyBoardKey.OnKeyPressed += KeyPressedCallBack;
+        GameManager.OnGameStateChanged += GameStateChangedCallBack;
+    }
+
+    void OnDestroy()
+    {
+        KeyBoardKey.OnKeyPressed -= KeyPressedCallBack;
+        GameManager.OnGameStateChanged -= GameStateChangedCallBack;
+    }
+
+    private void GameStateChangedCallBack(GameState gameState)
+    {
+       switch(gameState)
+        {
+            case GameState.Game:
+                Initialize();
+                break;
+
+            case GameState.LevelComplete:
+                break;
+        }
+    }
+
+    public void Initialize()
+    {
+        currentWordContainerIndex = 0;
+        canAddLetter = true;
+
+        DisableTryButton();
+        for (int i = 0; i < wordContainers.Length; i++)
+        {
+            wordContainers[i].Initialize();
+        }
+    }
+
+    private void KeyPressedCallBack(char letter)
+    {
+        if (!canAddLetter || currentWordContainerIndex >= wordContainers.Length)
+            return;
+
+        wordContainers[currentWordContainerIndex].Add(letter);
+
+        if (wordContainers[currentWordContainerIndex].IsComplete())
+        {
+            canAddLetter = false;
+            EnableTryButton();
+        }
+    }
+
+    public void CheckWord()
+    {
+        string wordToCheck = wordContainers[currentWordContainerIndex].GetWord();
+        string secretWord = WordManager.Instance.GetSecretWord();
+        
+        wordContainers[currentWordContainerIndex].Colorize(secretWord);
+        keyboardColorizer.Colorizer(secretWord, wordToCheck);
+        if (wordToCheck == secretWord)
+        {
+            SetLevelComplete();
+        }
+        else
+        {
+            Debug.Log("Wrong word");
+            currentWordContainerIndex++;
+            DisableTryButton();
+
+            if(currentWordContainerIndex >= wordContainers.Length)
+            {
+                GameManager.Instance.SetGameState(GameState.GameOver);
+                return;
+            } else
+            {
+                canAddLetter = true;
+            }
+        }
+    }
+
+    private void SetLevelComplete()
+    {
+        UpdateData();
+        GameManager.Instance.SetGameState(GameState.LevelComplete);
+    }
+
+    private void UpdateData()
+    {
+        int scoreToAdd = 6 - currentWordContainerIndex;
+
+        DataManager.instance.IncreaseScore(scoreToAdd);
+        DataManager.instance.AddCoin(scoreToAdd * 3);
+    }
+
+    public void BackSpacePressedCallBack()
+    {
+        //if(!GameManager.Instance.IsGameState())
+            //return;
+        bool removeLetter = wordContainers[currentWordContainerIndex].RemoveLetter();
+        if (!removeLetter)
+        {
+            DisableTryButton();
+        }
+        canAddLetter = true;
+    }
+
+    public void EnableTryButton()
+    {
+        tryButton.interactable = true;
+    }
+
+    public void DisableTryButton()
+    {
+        tryButton.interactable = false;
+    }
+}
---
+++ Guess word/Day5/UIManager.cs
@@ -0,0 +1,129 @@
+using UnityEngine;
+using TMPro;
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance { get; private set; }
+
+    [Header("UI Elements")]
+    [SerializeField] private CanvasGroup gameCG;
+    [SerializeField] private CanvasGroup levelCompleteGG;
+    
+    [Header("Level Complete UI")]
+    [SerializeField] private TextMeshProUGUI levelCompleteCoins;
+    [SerializeField] private TextMeshProUGUI levelCompleteSecretWord;
+    [SerializeField] private TextMeshProUGUI levelCompleteScore;
+    [SerializeField] private TextMeshProUGUI levelCompleteBestScore;
+
+    [Header("GameOver UI")]
+    [SerializeField] private TextMeshProUGUI gameOverCoin;
+    [SerializeField] private TextMeshProUGUI gameOverSecretWord;
+    [SerializeField] private TextMeshProUGUI gameOverBestScore;
+
+    [Header("Game Elements")]
+    [SerializeField] private TextMeshProUGUI gameScore;
+    [SerializeField] private TextMeshProUGUI gameCoins;
+
+    private void Start()
+    {
+        ShowGame();
+        HideLevelComplete ();
+        GameManager.OnGameStateChanged += GameStateChangedCallBack;
+    }
+
+    void OnDestroy()
+    {
+        GameManager.OnGameStateChanged -= GameStateChangedCallBack;
+    }
+
+
+    private void GameStateChangedCallBack(GameState gameState)
+    {
+        switch (gameState)
+        {
+            case GameState.Menu:
+                HideGame();
+                HideLevelComplete();
+                break;
+            case GameState.Game:
+                ShowGame();
+                HideLevelComplete();
+                break;
+            case GameState.Play:
+                ShowGame();
+                HideLevelComplete();
+                break;
+            case GameState.LevelComplete:
+                ShowLevelComplete();
+                HideGame();
+                break;
+            case GameState.GameOver:
+                ShowGameOver();
+                HideGame();
+                break;
+            case GameState.Idle:
+                HideGame();
+                HideLevelComplete();
+                break;
+        }
+    }
+
+    private void Awake()
+    {
+        // Ensure that there is only one instance of UIManager
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void ShowGame()
+    {
+        gameCoins.text = DataManager.instance.GetCoin().ToString();
+        gameScore.text = DataManager.instance.GetScore().ToString();
+        ShowCG(gameCG);
+    }
+
+    private void HideGame()
+    {
+        HideCG(gameCG);
+    }
+
+    private void ShowGameOver()
+    {
+        gameOverCoin.text = DataManager.instance.GetCoin().ToString();
+        gameOverSecretWord.text = DataManager.instance.SecretWord;
+        gameOverBestScore.text = DataManager.instance.GetBestScore().ToString();
+        ShowCG(levelCompleteGG);
+    }
+
+    private void ShowLevelComplete()
+    {
+        levelCompleteCoins.text = DataManager.instance.GetCoin().ToString();
+        levelCompleteSecretWord.text = DataManager.instance.SecretWord;
+        levelCompleteScore.text = DataManager.instance.GetScore().ToString();
+        levelCompleteBestScore.text = DataManager.instance.GetBestScore().ToString();
+        ShowCG(levelCompleteGG);
+    }
+
+    private void HideLevelComplete()
+    {
+        HideCG(levelCompleteGG);
+    }
+
+    private void ShowCG(CanvasGroup cg)
+    {
+        cg.alpha = 1;
+        cg.interactable = true;
+        cg.blocksRaycasts = true;
+    }
+    private void HideCG(CanvasGroup cg)
+    {
+        cg.alpha = 0;
+        cg.interactable = false;
+        cg.blocksRaycasts = false;
+    }
+}
